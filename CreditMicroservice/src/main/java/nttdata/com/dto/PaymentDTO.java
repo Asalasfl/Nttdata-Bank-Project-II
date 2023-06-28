bbd92475a54fc1aae99a64b3f130d6e77061c113
X12785 <asalasfl@emeal.nttdata.com>
2023-06-28 16:35:21 -0500

    update

---
+++ CreditMicroservice/src/main/java/nttdata/com/dto/PaymentDTO.java
@@ -0,0 +1,17 @@
+package nttdata.com.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+    private String id;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
+}
